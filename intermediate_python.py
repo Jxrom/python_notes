b03889d732bcc6cd24ab27ecf1141904d47fe4c6
Jxrom <68335788+Jxrom@users.noreply.github.com>
2022-10-28 09:30:10 +0800

    Update intermediate_python.py

--- intermediate_python.py
+++ intermediate_python.py
@@ -349,8 +349,6 @@ def countdown():
 for i in countdown():
     print(i)
 
-
-
 def add_num():
     for i in range(20):
         yield i 
@@ -364,11 +362,72 @@ def five(x):
         yield 5 * x
 
 for i in five(2):
-    print(i)
+    #print(i)
+    pass
 
-def infinite_spike():
-    while True:
-        yield "Spike!"
+"""
+    Finite generators can be converted into lists by passing them as 
+    arguments to the list function.
+"""
 
-for i in infinite_spike():
-    print(i)
+def numbers(x):
+    for i in range(x):
+        if i % 2 == 0:
+            yield i
+
+print(list(numbers(11)))
+
+"""
+    Using generators results in improved performance, which is the 
+    result of the lazy (on demand) generation of values, which translates
+    to lower memory usage. Furthermore, we do not need to wait until all 
+    the elements have been generated before we start to use them.
+"""
+
+def make_word():
+    word = ""
+    for ch in "spam":
+        word += ch
+        yield word
+    
+print(list(make_word()))    
+
+# DECORATORS
+
+"""
+    Decorators provide a way to modify functions using other functions.
+    This is ideal when you need to extend the functionality of functions
+    that you don't want to modify.
+"""
+
+def decor(func):
+    def wrap():
+        print("============")
+        func()
+        print("============")
+    return wrap
+
+def print_text():
+    print("Hello world!")
+
+decorated = decor(print_text)
+decorated()
+
+"""
+    We defined a function named decor that has a single parameter func.
+    Inside decor, we defined a nested function named wrap. The wrap function
+    will print a string, then call func(), and print another string. The
+    decor function returns the wrap function as its result.
+
+    We could say that the variable decorated is a decorated version of print_
+    text - it's print_text plus something.
+
+    In fact, if we wrote a useful decorator we might want to replace print_text
+    with the decorated version altogether so we always got our "plus something"
+    version of print_text.
+
+    This is done by re-assigning the variable that contains our function.
+"""
+
+print_text = decor(print_text)
+print_text()
