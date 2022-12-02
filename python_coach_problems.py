cf0fdb3323da707cadc4718b7a3336bf38967208
Jxrom <68335788+Jxrom@users.noreply.github.com>
2022-12-02 21:52:59 +0800

    December 2
    December 2, 2022

---
+++ python_coach_problems.py
@@ -0,0 +1,47 @@
+# Simple Operations
+
+total_secondsMonth = (((30 * 24) * 60) * 60)
+print(total_secondsMonth)
+
+# Exponentiation
+
+bacteria_exponentiation = 500*(2**24)
+print(bacteria_exponentiation)
+
+# Quotient & Remainder
+
+minutes = 888
+hours = minutes // 60
+remainder = minutes % 60
+
+print(hours)
+print(remainder)
+
+# Strings
+print("\"Stay hungry, stay foolish\" by Steve Jobs")
+
+# Newlines
+print("*\n**\n***\n****")
+
+# String Operations
+print("hi"*42)
+
+# Variables
+name = "James"
+age = "42"
+print(name + " is " + age + " years old")
+
+# Taking User Input
+user_inp = input()
+print("*** " + user_inp + " ***")
+
+# Working with Input
+name = input()
+age = input()
+print(name + " is "+age+" years old")
+
+# if Statement
+purity = float(input())
+if purity >= 91.7 or purity >= 99.9:
+    print("Accepted")
+
